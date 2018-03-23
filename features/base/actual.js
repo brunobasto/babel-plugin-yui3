406d8515635f03ce267500eb9f776a64479390b4
Bruno Basto <bruno.basto@liferay.com>
2018-03-23 01:05:29 -0300

    Test added features

--- features/base/actual.js
+++ features/base/actual.js
@@ -1,9 +1,37 @@
 'use strict';
 
 import 'base';
+import 'test';
 
-class MyClass extends Y.Base {
-  method(param) {
-    console.log('called method with param:', param);
-  }
+class MyParentClass {
+	static ATTRS = {
+		parentAttribute: {
+			value: 'parent'
+		}
+	};
+
+	superMethod() {
+		console.log('got parentAttribute', this.get('parentAttribute'));
+	}
 }
+
+class MyClass extends MyParentClass {
+	static ATTRS = {
+		childAttribute: {
+			value: 'child'
+		}
+	};
+
+	method(param) {
+		const arrowFunction = () => {
+			console.log('got childAttribute', this.get('childAttribute'));
+		};
+		arrowFunction();
+		super.superMethod(true, false);
+		console.log('called method with param:', param);
+	}
+}
+
+export default MyClass;
+export {MyParentClass, MyClass};
+
