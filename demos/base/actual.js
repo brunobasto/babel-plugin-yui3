2c7a7b2fd7708724fddfce07d759c4c35b44c2fa
Bruno Basto <bruno.basto@liferay.com>
2018-03-23 01:05:56 -0300

    generated

--- demos/base/actual.js
+++ demos/base/actual.js
@@ -1,15 +1,44 @@
 'use strict';
 
 YUI.add('actual', function (Y) {
-  var MyClass = function (_Y$Base) {
-    return Y.Base.create('MyClass', _Y$Base, [], {
-      method: function method(param) {
-        console.log('called method with param:', param);
-      }
-    }, {});
-  }(Y.Base);
+	var MyParentClass = MyParentClass = function () {
+		return Y.Base.create('MyParentClass', Y.Base, [], {
+			superMethod: function superMethod() {
+				console.log('got parentAttribute', this.get('parentAttribute'));
+			}
+		}, {
+			ATTRS: {
+				parentAttribute: {
+					value: 'parent'
+				}
+			}
+		});
+	}();
 
-  Y.MyClass = MyClass;
+	var MyClass = MyClass = function (_MyParentClass) {
+		return Y.Base.create('MyClass', _MyParentClass, [], {
+			method: function method(param) {
+				var _this = this;
+
+				var arrowFunction = function arrowFunction() {
+					console.log('got childAttribute', _this.get('childAttribute'));
+				};
+				arrowFunction();
+				MyClass.superclass.superMethod.apply(this, [true, false]);
+				console.log('called method with param:', param);
+			}
+		}, {
+			ATTRS: {
+				childAttribute: {
+					value: 'child'
+				}
+			}
+		});
+	}(MyParentClass);
+
+	Y.MyClass = MyClass
+	Y.MyParentClass = MyParentClass
+	Y.MyClass = MyClass
 }, '', {
-  'requires': ['base']
+	'requires': ['base', 'test']
 });
